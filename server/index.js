0a729f5e8352008d9ca27d2a52904199eefffbb3
abhi7889 <abhishek131214@gmail.com>
2023-12-24 21:47:15 +0530

    boilerplate server setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended:false}))
+
+app.get('/',(req,res) =>{
+    res.json({
+        status:'Sucess',
+        message:'All good!',
+    })
+})
+
+app.listen(process.env.port,()=>{
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>console.log('listening on port 4000'))
+    .catch(error => console.log(error))
+})
